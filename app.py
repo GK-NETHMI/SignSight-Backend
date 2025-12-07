b500e71390f3300f26ddbacf81a07248f0d662d7
Kaveesha <nethmikavee40@gmail.com>
2025-12-07 16:05:23 +0530

    install  flask packages

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request 
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["GET"]) 
 def home():
     return jsonify({"message": "Flask API is working!"})
 
